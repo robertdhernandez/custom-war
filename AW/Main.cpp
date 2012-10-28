d198396c8f65ce24b937e6d546b2292c673a3c66
Bobby Hernandez <killerjaguar26@gmail.com>
2012-10-28 19:58:29 -0400

    Test main class to experiment with listeners and controllers

--- AW/Main.cpp
+++ AW/Main.cpp
@@ -6,27 +6,58 @@
 
 #ifdef _WIN32
 #include <Windows.h>
+#include <iostream> //DEBUG
 #else
 #include <iostream>
 #endif
+
+#include "controller_key.h"
+#include "controller_mouse.h"
+#include "listener.h"
+
+class TestClass : 
+	public cw::util::KeyListener, 
+	public cw::util::KeyController
+{
+public:
+	TestClass()
+	{
+		addKeyListener( *this );
+	}
+
+	using cw::util::KeyController::updateKeyListeners;
+
+private:
+	void onKeyPressed( const sf::Event::KeyEvent& )
+	{
+		std::cout << "TestClass::onKeyPressed called!" << std::endl;
+	}
+
+	void onKeyReleased( const sf::Event::KeyEvent& )
+	{
+		std::cout << "TestClass::onKeyReleased called!" << std::endl;
+	}
+};
 
 int main( int argc, char* argv[] )
 {
 	try
 	{
+		TestClass test;
+
 		sf::RenderWindow window;
 		window.create( sf::VideoMode( 320, 240 ), "Custom Wars", sf::Style::Close );
 		window.setSize( sf::Vector2u( 640, 480 ) );
 		window.setPosition( window.getPosition() - sf::Vector2i( 160, 120 ) );
 
-		cw::Map map;
-		map.load( "map1.tmx" );
+		//cw::Map map;
+		//map.load( "map1.tmx" );
 
-		cw::Team team( 0 );
+		//cw::Team team( 0 );
 
-		std::unique_ptr< cw::Infantry > infantry[ 3 ];
-		for ( int i = 0; i < 3; i++ )
-			infantry[ i ].reset( new cw::Infantry( team, map.getTile( i, 0 ) ) );
+		//std::unique_ptr< cw::Infantry > infantry[ 3 ];
+		//for ( int i = 0; i < 3; i++ )
+		//	infantry[ i ].reset( new cw::Infantry( team, map.getTile( i, 0 ) ) );
 
 		while ( window.isOpen() )
 		{
@@ -35,12 +66,14 @@ int main( int argc, char* argv[] )
 			{
 				if ( events.type == sf::Event::Closed || ( events.type == sf::Event::KeyPressed && events.key.code == sf::Keyboard::Escape ) )
 					window.close();
+
+				test.updateKeyListeners( events );
 			}
 
 			window.clear();
 
-			for ( int i = 0; i < 3; i++ )
-				window.draw( *infantry[ i ] );
+			//for ( int i = 0; i < 3; i++ )
+			//	window.draw( *infantry[ i ] );
 
 			window.display();
 		}
