6f08595ff17d11a1c46127de4624ea331bb06298
Bobby Hernandez <killerjaguar26@gmail.com>
2012-11-04 22:18:40 -0500

    Added documentation of classes for Client and Host

--- AW/client.h
+++ AW/client.h
@@ -13,6 +13,30 @@ namespace cw
 
 	namespace net
 	{
+		//-------------------------------------------------------------------------
+		// [ABSTRACT CLASS]
+		//	Implemented classes gain the ability to connect to a remote server
+		//	A call to updateClient must be called every update cycle to recieve incoming packets
+		//	Sending data is done by making a serial::Packetstream and calling sendToHost
+		//
+		// VIRTUAL FUNCTIONS
+		//
+		//	void onClientConnect( serial::Packetstream& )
+		//		Called when the client connects for the first time with the server
+		//		Irregular packets may be sent the first time to synchronize the client to the host
+		//		See also: Host::onHostConnect
+		//
+		//	void onClientRecieve( serial::Packetstream& )
+		//		Called when the clients recieve a packet
+		//		It is the implementer's responsibility to read and use the packet
+		//
+		//	void onClientDisconnect()
+		//		Called when the client disconnects from the host, be it from losing connection to by choice
+		//
+		// NOTE
+		//	If an implementation inherits both Host and Client, they must also implement
+		//		void disconnect()
+		//-------------------------------------------------------------------------
 		class Client : public virtual NetBase
 		{
 		public:
--- AW/host.h
+++ AW/host.h
@@ -15,6 +15,31 @@ namespace cw
 
 	namespace net
 	{
+		//-------------------------------------------------------------------------
+		// [ABSTRACT CLASS]
+		//	Implemented classes gain the ability to host and recieve connections
+		//	When the host recieves a packet, it is then redistributed to all the clients
+		//	A call to updateHost must be made every update cycle to recieve packets
+		//
+		//	MAX CONNECTIONS: 255
+		//
+		// VIRTUAL FUNCTIONS
+		//
+		//	void onHost()
+		//		Called when the server begins hosting
+		//
+		//	void onHostConnect( sf::TcpSocket& )
+		//		Called when the host recieves a new connection
+		//		Implementations should send special packet data to initialize the client and synchronize them
+		//		See also: Client::onClientConnect
+		//
+		//	void onHostDisconnect()
+		//		Called when the host stops hosting
+		//
+		// NOTE
+		//	If an implementation inherits both Host and Client, they must also implement
+		//		void disconnect()
+		//-------------------------------------------------------------------------
 		class Host : public virtual NetBase
 		{
 		public:
